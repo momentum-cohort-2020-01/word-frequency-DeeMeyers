e341b1fe28faedfebdc5fe8cecbf303cc0a320dd
DeeMeyers <justdmeyers@gmail.com>
2020-02-18 01:49:28 -0500

    need to figure out file to string and then making the display

--- word_frequency.py
+++ word_frequency.py
@@ -5,66 +5,69 @@ STOP_WORDS = [
 ]
 
 
-# def print_word_freq(file):
 #     """Read in `file` and print out the frequency of words in that file."""
 #     pass
 
 
-# if __name__ == "__main__":
-#     import argparse
-#     from pathlib import Path
 
-#     parser = argparse.ArgumentParser(
-#         description='Get the word frequency in a text file.')
-#     parser.add_argument('file', help='file to read')
-#     args = parser.parse_args()
+def print_word_freq(file):
+    #take original text input
+    input = 'place holder'
+    # input2 = 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.'
+    #makes original text lowercase
+    input = input.lower()
+    #list of punctuation
+    punc = '"!@#$%^&*()_-<>+=,./'
+    #removes punctuation
+    cleaninput = ''
+    for char in input:
+        if char not in punc:
+            cleaninput = cleaninput + char
+            cleaninput = cleaninput.split(" ")
+    #removes stop words
+    nostopinputclean = ""
+    for each in cleaninput:
+        if each not in STOP_WORDS:
+            nostopinputclean = nostopinputclean + " " + each
 
-#     file = Path(args.file)
-#     if file.is_file():
-#         print_word_freq(file)
-#     else:
-#         print(f"{file} does not exist!")
-#         exit(1)
-#take original text input
-input = 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.'
-#makes original text lowercase
-input = input.lower()
-#list of punctuation
-punc = '"!@#$%^&*()_-<>+=,./'
-#removes punctuation
-cleaninput = ''
-for char in input:
-  if char not in punc:
-    cleaninput = cleaninput + char
-cleaninput = cleaninput.split(" ")
-#removes stop words
-nostopinputclean = ""
-for each in cleaninput:
-    if each not in STOP_WORDS:
-        nostopinputclean = nostopinputclean + " " + each
+    nostopinputclean = nostopinputclean.split(" ")
+    nostopinputclean.pop(0)
+    print("nostopinputclean: ")
+    print(nostopinputclean)
 
-nostopinputclean = nostopinputclean.split(" ")
-nostopinputclean.pop(0)
-print("nostopinputclean: ")
-print(nostopinputclean)
+    dictList = ['']
 
-dictList = ['']
-
-for each in nostopinputclean:
-  if each not in dictList:
-    dictList.append(each)
+    for each in nostopinputclean:
+        if each not in dictList:
+            dictList.append(each)
 
 
-dictList.pop(0)
-print(dictList)
+    dictList.pop(0)
+    print(dictList)
 
-dictionary = {}    
-for each in dictList:
-  dictionary.update({each: 0}) 
+    dictionary = {}    
+    for each in dictList:
+        dictionary.update({each: 0}) 
 
 
-for each in nostopinputclean:
-  dictionary[each] += 1
+    for each in nostopinputclean:
+        dictionary[each] += 1
 
-print(dictionary)
+    print(dictionary)
 
+
+if __name__ == "__main__":
+    import argparse
+    from pathlib import Path
+
+    parser = argparse.ArgumentParser(
+        description='Get the word frequency in a text file.')
+    parser.add_argument('file', help='file to read')
+    args = parser.parse_args()
+
+    file = Path(args.file)
+    if file.is_file():
+        print_word_freq(file)
+    else:
+        print(f"{file} does not exist!")
+        exit(1)
